4fe490e77e4c5709fc71f3da220582520d7a7228
Leigh Rubin <leighrub@usc.edu>
2018-12-16 14:57:54 -0600

    form and js to hide and show + navbar

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
 var stripe = Stripe('pk_test_YWgCIeFFJkMPBXQwYiP55gyt');
 var elements = stripe.elements();
+
+function showEmailForm() {
+  var form = document.getElementById("form");
+  form.style.display = "block";
+}
+
+function hideEmialForm() {
+  var form = document.getElementById("form");
+  form.style.display = "none";
+}
